bb01b4cfa15771268927eb246b1e90ffbccb898b
Alex Muravyov <alex.muravyov@gmail.com>
2023-03-07 17:51:55 +0300

    Cloud Configuration 2

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => [
     "strapi::errors",
     "strapi::security",
-    // {
-    //     name: "strapi::security",
-    //     config: {
-    //         contentSecurityPolicy: {
-    //             directives: {
-    //                 "default-src": ["'self'"],
-    //                 "img-src": ["'self'", "data:", "blob:", env("SUPABASE_API_URL")],
-    //             },
-    //         },
-    //     },
-    // },
+    {
+        name: "strapi::security",
+        config: {
+            contentSecurityPolicy: {
+                directives: {
+                    "default-src": ["'self'"],
+                    "img-src": ["'self'", "data:", "blob:", env("SUPABASE_API_URL")],
+                },
+            },
+        },
+    },
     "strapi::cors",
     "strapi::poweredBy",
     "strapi::logger",
